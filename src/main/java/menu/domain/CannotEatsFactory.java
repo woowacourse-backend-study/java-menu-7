0c6286115af7ccecd1fe9ee6b3859a8b13415683
pakaheo <k70923@konkuk.ac.kr>
2024-11-16 15:27:47 +0900

    feat : 못 먹는 음식은 콤마로 구분

---
+++ src/main/java/menu/domain/CannotEatsFactory.java
@@ -0,0 +1,24 @@
+package menu.domain;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CannotEatsFactory {
+
+    private static final String COACH_NAME_REGEX = "(\\w+)(,\\w+)*";
+    private static final Pattern PATTERN = Pattern.compile(COACH_NAME_REGEX);
+
+    public static List<String> createCannotEats(String eats) {
+        Matcher coachMatcher = PATTERN.matcher(eats);
+
+        if (!coachMatcher.find()) {
+            throw new IllegalArgumentException("[ERROR] 못 먹는 음식은 콤마로 구분해야 합니다.");
+        }
+
+        String[] result = eats.split(",");
+
+        return Arrays.asList(result);
+    }
+}
