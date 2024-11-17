274e8a1fe3db89208af45c4ee761cae5cde7fc11
pakaheo <k70923@konkuk.ac.kr>
2024-11-17 22:22:34 +0900

    docs : 기능 구현 목록 작성

---
+++ src/main/java/menu/domain/HateFoodsParser.java
@@ -0,0 +1,34 @@
+package menu.domain;
+
+import java.util.Arrays;
+import java.util.List;
+import menu.error.ErrorMessage;
+
+public class CoachNameParser {
+
+    private static final String COACH_NAME_REGEX = "([가-힣]+)(,[가-힣]+)*";
+    private static final String SEPARATOR = ",";
+    private static final int MINIMUM_NUMBER_OF_COACHES = 2;
+    private static final int MAXIMUM_NUMBER_OF_COACHES = 2;
+
+    public static List<String> parseNameInput(String nameInput) {
+        checkDividedSeparator(nameInput);
+
+        String[] names = nameInput.split(SEPARATOR);
+        checkNumberOfCoaches(names);
+
+        return Arrays.asList(names);
+    }
+
+    private static void checkNumberOfCoaches(String[] names) {
+        if (names.length < MINIMUM_NUMBER_OF_COACHES || names.length > MAXIMUM_NUMBER_OF_COACHES) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_NUMBER_OF_COACH.valueOf());
+        }
+    }
+
+    private static void checkDividedSeparator(String nameInput) {
+        if (!nameInput.matches(COACH_NAME_REGEX)) {
+            throw new IllegalArgumentException(ErrorMessage.UNDIVIDED_NAME_BY_COMMA.valueOf());
+        }
+    }
+}
---
+++ src/test/java/menu/domain/HateFoodsParserTest.java
@@ -0,0 +1,33 @@
+package menu.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import menu.error.ErrorMessage;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class CoachNameParserTest {
+
+    @Test
+    void 코치_이름은_콤마로_구분해야_한다() {
+        assertThat(CoachNameParser.parse("포비,나나")).contains("포비", "나나");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"포비:나나", ",포비,나나", "포비,나나,"})
+    void 코치_이름이_콤마로_구분되지_않으면_예외(String input) {
+        assertThatThrownBy(() -> CoachNameParser.parse(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.UNDIVIDED_NAME_BY_COMMA.valueOf());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"포비", "포비,가가,나나,다다,라라,마마"})
+    void 코치는_2명_이상_5명_이하여야_한다(String input) {
+        assertThatThrownBy(() -> CoachNameParser.parse(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.INVALID_NUMBER_OF_COACH.valueOf());
+    }
+}
