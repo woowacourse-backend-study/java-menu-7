e1d2b3bdcaf6614590ad688679c9ae4f39d3d4e1
pakaheo <k70923@konkuk.ac.kr>
2024-11-17 23:22:03 +0900

    refactor : 코치 이름 유효성 검증 책임 이동

--- src/main/java/menu/domain/Coach.java
+++ src/main/java/menu/domain/Coach.java
@@ -3,28 +3,17 @@ package menu.domain;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-import menu.error.ErrorMessage;
 
 public class Coach {
-
-    private static final int MINIMUM_NAME_LENGTH = 2;
-    private static final int MAXIMUM_NAME_LENGTH = 4;
 
     private final String name;
     private final List<String> hateFoods;
     private final List<String> pickedMenus;
 
     public Coach(final String name, final List<String> hateFoods) {
-        validate(name);
         this.name = name;
         this.hateFoods = hateFoods;
         this.pickedMenus = new ArrayList<>();
-    }
-
-    private void validate(String name) {
-        if (name.length() < MINIMUM_NAME_LENGTH || name.length() > MAXIMUM_NAME_LENGTH) {
-            throw new IllegalArgumentException(ErrorMessage.INVALID_COACH_NAME_LENGTH.valueOf());
-        }
     }
 
     public boolean canAddMenu(String menuName) {
--- src/main/java/menu/domain/parser/CoachNameParser.java
+++ src/main/java/menu/domain/parser/CoachNameParser.java
@@ -10,14 +10,29 @@ public class CoachNameParser {
     private static final String SEPARATOR = ",";
     private static final int MINIMUM_NUMBER_OF_COACHES = 2;
     private static final int MAXIMUM_NUMBER_OF_COACHES = 5;
+    private static final int MINIMUM_NAME_LENGTH = 2;
+    private static final int MAXIMUM_NAME_LENGTH = 4;
 
     public static List<String> parse(String nameInput) {
         checkDividedSeparator(nameInput);
 
         String[] names = nameInput.split(SEPARATOR);
         checkNumberOfCoaches(names);
+        checkCoachName(names);
 
         return Arrays.asList(names);
+    }
+
+    private static void checkCoachName(String[] names) {
+        for (String name : names) {
+            checkLength(name);
+        }
+    }
+
+    private static void checkLength(String name) {
+        if (name.length() < MINIMUM_NAME_LENGTH || name.length() > MAXIMUM_NAME_LENGTH) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_COACH_NAME_LENGTH.valueOf());
+        }
     }
 
     private static void checkNumberOfCoaches(String[] names) {
